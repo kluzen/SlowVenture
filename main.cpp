29fac923197dfedec583ef9368d5aabad4bc919d
Ben Klutzke <benklutzke@gmail.com>
2014-11-16 21:06:25 -0500

    Just the beginning

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "rapidxml-1.13/rapidxml.hpp"
+using namespace std;
+using namespace rapidxml;
+
+int main () {
+	string line;
+	string content;
+	ifstream myfile;
+	myfile.open ("samples/sample.txt.xml");
+	if(myfile.is_open()){
+	  while(getline(myfile, line)){
+		  content += line + '\n';
+	  }
+	  myfile.close();
+	}
+
+	xml_document<> doc;
+	doc.parse<0>((char*)content.c_str());
+
+
+
+	cout << doc.first_node()->first_node()->name() << endl;
+	return 0;
+}
